0ee57907b66d99620999dc9a61be3fff188d837e
vboxuser <vboxuser@WIN10>
2025-02-13 18:55:06 +0300

    Add project files.

---
+++ FormsApp/PointForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using PointLib;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+
+namespace FormsApp
+{
+    public partial class PointForm : Form
+    {
+        private Point[] points = null;
+        public PointForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            points = new Point[5];
+
+            var rnd = new Random();
+
+            for (int i = 0; i < points.Length; i++)
+                points[i] = rnd.Next(3) % 2 == 0 ? new Point() : new Point3D();
+
+            listBox.DataSource = points;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (points == null)
+                return;
+
+            Array.Sort(points);
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+        }
+        private void btnSerialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|YAML|*.yaml|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs = new FileStream(dlg.FileName, FileMode.Create, FileAccess.Write))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        bf.Serialize(fs, points);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        sf.Serialize(fs, points);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        xf.Serialize(fs, points);
+                        break;
+                    case ".json":
+                        using (var w = new StreamWriter(fs))
+                        {
+                            var jf = new JsonSerializer();
+                            jf.Serialize(w, points);
+                        }
+                        break;
+                    case ".yaml":
+                        var serializer = new SerializerBuilder()
+                            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                            .Build();
+                        using (var w = new StreamWriter(fs))
+                        {
+                            string yaml = serializer.Serialize(points);
+                            w.Write(yaml);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void btnDeserialize_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "SOAP|*.soap|XML|*.xml|JSON|*.json|YAML|*.yaml|Binary|*.bin";
+
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (var fs = new FileStream(dlg.FileName, FileMode.Open, FileAccess.Read))
+            {
+                switch (Path.GetExtension(dlg.FileName))
+                {
+                    case ".bin":
+                        var bf = new BinaryFormatter();
+                        points = (Point[])bf.Deserialize(fs);
+                        break;
+                    case ".soap":
+                        var sf = new SoapFormatter();
+                        points = (Point[])sf.Deserialize(fs);
+                        break;
+                    case ".xml":
+                        var xf = new XmlSerializer(typeof(Point[]), new[] { typeof(Point3D) });
+                        points = (Point[])xf.Deserialize(fs);
+                        break;
+                    case ".json":
+                        using (var r = new StreamReader(fs))
+                        {
+                            string json = r.ReadToEnd();
+                            points = JsonConvert.DeserializeObject<Point3D[]>(json);
+                        }
+                        break;
+                    case ".yaml":
+                        using (var r = new StreamReader(fs))
+                        {
+                            var deserializer = new DeserializerBuilder()
+                                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                                .Build();
+                            points = deserializer.Deserialize<Point[]>(r);
+                        }
+                        break;
+                }
+            }
+
+            listBox.DataSource = null;
+            listBox.DataSource = points;
+        }
+    }
+}
---
+++ PointLib/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PointLib")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PointLib")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1b785477-926b-4a55-8290-ac3af6964c8f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
